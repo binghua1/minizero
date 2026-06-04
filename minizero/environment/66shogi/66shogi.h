0cbbeec05b41be9a61c5ad916c802aa17d5004b2
binghua <roybbc310@gmail.com>
2026-06-04 17:12:05 +0800

    66shogi init file

---
+++ minizero/environment/66shogi/66shogi.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "base_env.h"
+#include <string>
+#include <vector>
+
+namespace minizero::env::shogi66{
+
+const std::string kTicTacToeName = "66shogi";
+const int kshogi66NumPlayer = 2;
+const int kshogi66BoardSize = 6;
+
+enum class Phase {
+    kSetup,
+    kPlay,
+};
+
+enum class PieceType {
+    kEmpty,kKing,kGold,kSilver,kKnight,kLance,kRook,kBishop,kPawn,kPromSilver,kPromKnight,kPromLance,kHorse,kDragon,kTokin,
+};
+
+struct Piece {
+    Player owner;
+    PieceType type;
+};
+
+} // namespace minizero::env::shogi66
+
+// class TicTacToeEnv : public BaseBoardEnv<TicTacToeAction> {
+// public:
+//     TicTacToeEnv() : BaseBoardEnv<TicTacToeAction>(kTicTacToeBoardSize) { reset(); }
+
+//     void reset() override;
+//     bool act(const TicTacToeAction& action) override;
+//     bool act(const std::vector<std::string>& action_string_args) override;
+//     std::vector<TicTacToeAction> getLegalActions() const override;
+//     bool isLegalAction(const TicTacToeAction& action) const override;
+//     bool isTerminal() const override;
+//     float getReward() const override { return 0.0f; }
+//     float getEvalScore(bool is_resign = false) const override;
+//     std::vector<float> getFeatures(utils::Rotation rotation = utils::Rotation::kRotationNone) const override;
+//     std::vector<float> getActionFeatures(const TicTacToeAction& action, utils::Rotation rotation = utils::Rotation::kRotationNone) const override;
+//     inline int getNumInputChannels() const override { return 4; }
+//     inline int getPolicySize() const override { return getBoardSize() * getBoardSize(); }
+//     std::string toString() const override;
+//     inline std::string name() const override { return kTicTacToeName; }
+//     inline int getNumPlayer() const override { return kTicTacToeNumPlayer; }
+//     inline int getRotatePosition(int position, utils::Rotation rotation) const override { return utils::getPositionByRotating(rotation, position, getBoardSize()); };
+//     inline int getRotateAction(int action_id, utils::Rotation rotation) const override { return getRotatePosition(action_id, rotation); };
+//     static void setUpEnv() { config::env_board_size = 3; }
+
+// private:
+//     Player eval() const;
+
+//     std::vector<Player> board_;
+// };
+
+// class TicTacToeEnvLoader : public BaseBoardEnvLoader<TicTacToeAction, TicTacToeEnv> {
+// public:
+//     std::vector<float> getActionFeatures(const int pos, utils::Rotation rotation = utils::Rotation::kRotationNone) const override;
+//     inline std::vector<float> getValue(const int pos) const { return {getReturn()}; }
+//     inline std::string name() const override { return kTicTacToeName; }
+//     inline int getPolicySize() const override { return getBoardSize() * getBoardSize(); }
+//     inline int getRotatePosition(int position, utils::Rotation rotation) const override { return utils::getPositionByRotating(rotation, position, getBoardSize()); };
+//     inline int getRotateAction(int action_id, utils::Rotation rotation) const override { return getRotatePosition(action_id, rotation); };
+// };
+
